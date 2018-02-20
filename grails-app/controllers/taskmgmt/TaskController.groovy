517d067afb196b8d7b5b29ab8a843e66806f6d18
Sabin Shrestha <43sanrizz@gmail.com>
2018-02-19 21:38:36 -0700

    Made changes to unnecsarry controlls

--- grails-app/controllers/taskmgmt/TaskController.groovy
+++ grails-app/controllers/taskmgmt/TaskController.groovy
@@ -2,41 +2,8 @@ package taskmgmt
 
 class TaskController {
 
-    /**
-     * display the list of tasks
-     * @return
-     */
     def list() {
-        // added a comment
-        render view: "list",
-                model:
-                        [
-                                tasks: Task.list()
-                        ]
+        render view: "list", model:[tasks: Task.list()]
     }
-
-    def detail(){
-
-    }
-
-    def edit(){
-        
-    }
-
-
-    def tada() {
-        //tada() is when employee gets boored with doing what they are supposed to do and get some fun time
-        redirect action: "list"
-        //Why are you here if you don't wana work.
-        //you're fired
-    }
-
-    def listssss(){
-
-    }
-    def update(){
-        render view: "list"
-    }
-def su(){sdufsdifhiojas}
-    }
+}
 
