eb3b323cb0964f17c97d873fd60cc9139bb5540f
anilm <mandanil@isu.edu>
2018-02-26 09:58:38 -0700

    refactored Tasktype to TaskType, added new properties to Task domain

--- grails-app/domain/taskmgmt/Task.groovy
+++ grails-app/domain/taskmgmt/Task.groovy
@@ -6,16 +6,25 @@ class Task {
 
     String title
     String detail
-    Date dateCreated
-    Date dateModified
-    String flag
 
     TaskStatus taskStatus = TaskStatus.CREATED
+    // TODO create a new enum for TaskPriority
+
+    TaskType taskType           // this creates a belongsTo relationship with TaskType
+
+    Date dateCreated
+    Date dateModified
+    String flag                 // what is this for?? AM 02/26/2018
 
     static constraints = {
         taskStatus nullable: true
         dateCreated nullable: true
         dateModified nullable: true
         flag nullable: true
+        taskType nullable: true
+    }
+
+    static mapping = {
+        detail type: "text"             // this allows to enter text more than 255 characters
     }
 }
--- src/test/groovy/taskmgmt/TaskTypeSpec.groovy
+++ src/test/groovy/taskmgmt/TaskTypeSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Tasktype)
-class TasktypeSpec extends Specification {
+@TestFor(TaskType)
+class TaskTypeSpec extends Specification {
 
     def setup() {
     }
