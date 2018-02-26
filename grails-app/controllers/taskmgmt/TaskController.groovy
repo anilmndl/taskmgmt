3d91c305c0483ae8a09cbf09554feb18bf8460d0
pathsush <pathsush@isu.edu>
2018-02-25 17:22:25 -0700

    merge develop branch into Sushil

--- grails-app/controllers/taskmgmt/TaskController.groovy
+++ grails-app/controllers/taskmgmt/TaskController.groovy
@@ -27,17 +27,14 @@ class TaskController {
     def list() {
         render view: "list.gsp", model: [tasks: Task.list()]
     }
-
     def create() {
         // Task task=Task.get(params.id)
         render view: "create.gsp"
     }
-
     def detail() {
         Task tasks = Task.get(params.id)
         render view: "detail.gsp", model: [tasks: tasks]
     }
-
     def save(Task taskmgmt) {
         taskService.save(taskmgmt)
         redirect action: "list"
