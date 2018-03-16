29fe1083a059df79b271bba92b769b3f454b6e83
Sabin Shrestha <43sanrizz@gmail.com>
2018-03-16 17:44:18 -0600

    Solved issue #80

--- grails-app/controllers/taskmgmt/TaskController.groovy
+++ grails-app/controllers/taskmgmt/TaskController.groovy
@@ -35,7 +35,6 @@ class TaskController {
 
     def create() {
         //taskService?.createTask()
-
         // Task task=Task.get(params.id)
 
         render view: "create", model: [taskTypeList: TaskType.findAllByDateDeletedIsNull([sort: "dateCreated", order: "desc"]), userList: Users.list()]
--- grails-app/domain/taskmgmt/Task.groovy
+++ grails-app/domain/taskmgmt/Task.groovy
@@ -14,26 +14,19 @@ class Task {
 
     TaskType taskType           // this creates a belongsTo relationship with TaskType
 
-    //String taskTypeName
-
     Users users
 
-    String taskTypeName
     Date dateCreated
     Date dateModified
     Date dateCompleted          // the date when the task is marked completed
     Date dateDeleted            // use this to indicate if the task was deleted
 
-    String flag                 // what is this for?? AM 02/26/2018
 
     static constraints = {
-        taskTypeName nullable: true
         taskStatus nullable: true
-        dateCreated nullable: true
         dateModified nullable: true
         dateCompleted nullable: true
         dateDeleted nullable: true
-        flag nullable: true
         taskType nullable: true
         taskPriority nullable: true
     }
