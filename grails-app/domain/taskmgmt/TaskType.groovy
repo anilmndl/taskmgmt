6cf065fcd47b7c90968e597fa0a5607c602e188d
pokhalan <35437495+pokhalan@users.noreply.github.com>
2018-03-19 21:38:04 -0600

    minor changes

--- grails-app/domain/taskmgmt/TaskType.groovy
+++ grails-app/domain/taskmgmt/TaskType.groovy
@@ -5,7 +5,6 @@ class TaskType {
     String title
     String identifier    // use this to find specific task type. The title may change in future but identifier should never change
     String description   //description of the task type
-
     Date dateCreated
     Date dateModified
     Date dateDeleted
