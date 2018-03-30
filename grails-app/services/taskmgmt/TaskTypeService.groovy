92efe70cb02216dd3d560270acc085cef69180fb
pokhalan <35437495+pokhalan@users.noreply.github.com>
2018-03-30 12:21:38 -0600

    Issue #59 fixed

--- grails-app/services/taskmgmt/TaskTypeService.groovy
+++ grails-app/services/taskmgmt/TaskTypeService.groovy
@@ -6,8 +6,8 @@ import grails.transaction.Transactional
 class TaskTypeService {
 
     def save(TaskType taskType) {
+        taskType.identifier = slugify(taskType.title)
         taskType.dateCreated = new Date()
-        taskType.identifier = taskType.title
         if (taskType.validate()) {
             taskType.save failOnError: true, flush: true
         } else {
@@ -31,6 +31,13 @@ class TaskTypeService {
         } else {
             throw new Exception("Oops! Something went wrong. Deletion failed.")
         }
+    }
+
+    def slugify(String data) {
+        String slug = data
+        slug = slug.toLowerCase();
+        slug = slug.replaceAll(" ", "-")
+        return slug
     }
 
 }
